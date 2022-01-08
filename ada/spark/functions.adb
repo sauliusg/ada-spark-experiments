850c29a5cf27676f6eb49f415cd48436c10c98e2
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-01-08 20:09:43 +0000

    ada/ (saulius@starta) Proving post-condition using loop invariant.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@25 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/spark/functions.adb
+++ ada/spark/functions.adb
@@ -2,7 +2,7 @@
 function Functions ( A, B : Integer ) return Integer
   with Spark_Mode,
   Pre  => A > 0 and B > 0,
-  Post => Functions'Result >= 0
+  Post => Functions'Result > 0
 is
    I, J : Integer;
 begin
@@ -12,18 +12,12 @@ begin
    pragma Assert ( J > 0 );
    
    while I /= J loop
-      pragma Assert (I > 0);
-      pragma Assert (J > 0);
+      pragma Loop_Invariant (I > 0 and J > 0);
       if I > J then
-         pragma Assert (I > J);
          I := I - J;
-         pragma Assert (I > 0);
       else 
-         pragma Assert (I < J);
          J := J - I;
-         pragma Assert (J > 0);
       end if;
-      pragma Assert (I > 0 and J > 0);
    end loop;
    
    return (if I > J then I else J);
