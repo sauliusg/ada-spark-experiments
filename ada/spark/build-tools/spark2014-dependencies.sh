188b844ccb7517d6d2d24c81bbfa70f765023a1e
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-01-08 11:48:43 +0000

    ada/spark/ (saulius@starta) Adding history and dependency installation comamnds for Spark2014.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@19 af456918-7c63-4f43-8f72-c3d2023c81aa

---
+++ ada/spark/build-tools/spark2014-dependencies.sh
@@ -0,0 +1,18 @@
+sudo apt-get install -y gnat
+sudo apt-get install -y gnat-dev
+sudo apt-get install -y libisl-dev
+sudo apt-get install -y libmpc-dev
+sudo apt-get install -y libmpfr-dev 
+sudo apt-get install -y agda coq-theories coq 
+sudo apt-get install -y antlr 
+sudo apt-get install -y coq-dev 
+sudo apt-get install -y gnat
+sudo apt-get install -y gnat-9
+sudo apt-get install -y isabelle
+sudo apt-get install -y libgmp libmpfr libmpc
+sudo apt-get install -y libgmp-dev
+sudo apt-get install -y libmenhir-ocaml-dev
+sudo apt-get install -y libnum-ocaml-dev
+sudo apt-get install -y libyojson-ocaml-dev
+sudo apt-get install -y python-is-python3
+sudo apt-get install -y python3-toml
