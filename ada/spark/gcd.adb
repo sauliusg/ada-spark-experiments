e1aa09f81097daa37704733028be9b413abc2e26
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-01-08 20:02:22 +0000

    ada/ (saulius@starta) Tring to prove correctness of the GCD algorithm. Staring with some simple assertions.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@24 af456918-7c63-4f43-8f72-c3d2023c81aa

---
+++ ada/spark/gcd.adb
@@ -0,0 +1,23 @@
+package body GCD with Spark_Mode Is
+   
+   function GCD (A, B : in Integer) return Integer
+   is
+      X, Y : Integer;
+   begin
+      pragma Assume (A > 0 and B > 0);
+      X := A;
+      Y := B;
+      pragma Assert (X > 0 and Y > 0);
+      
+      while X /= Y loop
+         if X > Y then
+            X := X - Y;
+         else
+            Y := Y - X;
+         end if;
+      end loop;
+      
+      return X;
+   end;
+   
+end GCD;
