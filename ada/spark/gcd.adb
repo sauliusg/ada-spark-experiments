63a2969fd1fef171356b0ee96a06ee317bb6afbc
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-01-09 13:16:19 +0000

    ada/ (saulius@starta) Can not prove distributivity of 'mod', even with the Assume pragma...
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@43 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/spark/gcd.adb
+++ ada/spark/gcd.adb
@@ -1,14 +1,55 @@
+pragma Ada_2022;
+
+with Ada.Numerics.Big_Numbers.Big_Integers;
+use  Ada.Numerics.Big_Numbers.Big_Integers;
+  
 package body GCD with Spark_Mode Is
    
-   function GCD (A, B : in Integer) return Integer
+   procedure Lemma_Mod_0 (X,Y,D : in out Positive) 
+     with 
+     Ghost,
+     Pre => X in Positive and then Y in Positive and then D in Positive,
+     Post => (if X mod D = 0 and then Y mod D = 0 then 
+     X mod D - Y mod D = 0)
    is
-      X, Y : Integer;
+   begin
+      null;
+   end;
+   
+   pragma Assume ( for all X in Positive =>
+                     (for all Y in Positive =>
+                        (for all D in Positive =>
+                           (X mod D - Y mod D) mod D = (X-Y) mod D
+                        )
+                     )
+                 );
+   
+   procedure Lemma_Mod_Distributivity (X,Y,D : in out Positive) 
+     with 
+     Ghost,
+     Pre => X in Positive and then Y in Positive and then D in Positive,
+     Post => (if X > Y then (X mod D - Y mod D) mod D = (X-Y) mod D)
+   is
+   begin
+      null;
+   end;
+   
+   function GCD (A, B : in Positive) return Positive
+   is
+      X, Y : Positive;
    begin
       X := A;
       Y := B;
       
       while X /= Y loop
          pragma Loop_Invariant (X > 0 and Y > 0);
+         pragma Loop_Invariant (
+                                for all D in Positive =>
+                                  (if X mod D = 0 and then
+                                     Y mod D = 0 and then
+                                     X > Y
+                                     then X mod D - Y mod D = 0)
+                               );
          if X > Y then
             X := X - Y;
          else
--- ada/spark/gcd.ads
+++ ada/spark/gcd.ads
@@ -4,24 +4,20 @@ with Ada.Numerics.Big_Numbers.Big_Integers;
 use  Ada.Numerics.Big_Numbers.Big_Integers;
   
 package GCD with Spark_Mode Is
-   
-   function Max (A, B : Integer) return Integer 
-   is (if A > B then A else B)
+
+   function Divisor (A,D : Positive) return Boolean
+     is (A mod D = 0)
      with
-     Ghost,
-     Pre => A in Integer and then B in Integer, 
-     Post => Max'Result = (if A > B then A else B);       
-     
-   function GCD (A, B : in Integer) return Integer
+       Ghost,
+       Pre => A in Positive and then D in Positive,
+       Post => Divisor'Result = (A mod D = 0);
+   
+   function GCD (A, B : in Positive) return Positive
      with
      Pre  => 
-       A > 0 and then A <= Integer'Last and then 
-       B > 0 and then B <= Integer'Last,
-     Post => 
-         (for some D in 1 .. Max (A,B) => 
-            To_Big_Integer (GCD'Result) * To_Big_Integer (D) = 
-            To_Big_Integer (A)
-         );
+       A > 0 and then A <= Positive'Last and then 
+       B > 0 and then B <= Positive'Last,
+     Post => Divisor (A, GCD'Result);
          
      -- The following postcondition can not be (easily?) proved in
      -- 'gnatprove':
