f04ddcc227bf03e56095b442d8f04105ed3c0131
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-01-09 10:23:56 +0000

    ada/ (saulius@starta) Adding function Max as 'Ghost' and 'Import'; formulating postcondition of GCD with quantifiers instead of 'mod' operation.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@32 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/spark/gcd.ads
+++ ada/spark/gcd.ads
@@ -1,13 +1,26 @@
 package GCD with Spark_Mode Is
-
+   
+   function Max (A, B : Integer) return Integer 
+   is (if A > B then A else B)
+     with
+     Ghost, Import,
+     Pre => A in Integer and then B in Integer, 
+     Post => Max'Result = (if A > B then A else B);       
+     
    function GCD (A, B : in Integer) return Integer
      with
      Pre  => 
        A > 0 and then A <= Integer'Last and then B > 0 and then
        B <= Integer'Last,
-     Post =>
-       GCD'Result > 0 and then
-       A mod GCD'Result = 0 and then
-       B mod GCD'Result = 0;
+     Post => 
+       not (for all D in 2 .. Max (A,B) => GCD'Result * D /= A);
+         
+     -- The following postcondition can not be (asily?) proved in
+     -- 'gnatprove':
+     -- 
+     -- Post =>
+     --   GCD'Result > 0 and then
+     --   A mod GCD'Result = 0 and then
+     --   B mod GCD'Result = 0;
    
 end GCD;
