dda18bb7a156f50f36f777415a65ed289ba84471
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-01-09 11:49:42 +0000

    ada/ (saulius@starta) Adding technical run of SPARK lemmas.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@39 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/spark/gcd.adb
+++ ada/spark/gcd.adb
@@ -1,11 +1,26 @@
+pragma Ada_2022;
+
+with Ada.Numerics.Big_Numbers.Big_Integers;
+use Ada.Numerics.Big_Numbers.Big_Integers;
+
+with SPARK.Arithmetic_Lemmas; 
+
 package body GCD with Spark_Mode Is
    
+   package Integer_Arithmetic_Lemmas
+   is new SPARK.Arithmetic_Lemmas (Int => Integer,
+                                   Big => To_Big_Integer);
+
+   use Integer_Arithmetic_Lemmas;
+  
    function GCD (A, B : in Integer) return Integer
    is
       X, Y : Integer;
    begin
       X := A;
       Y := B;
+      
+      Lemma_Div_Is_Monotonic (A, B, X);
       
       while X /= Y loop
          pragma Loop_Invariant (X > 0 and Y > 0);
