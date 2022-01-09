30a95466944e0dd82c2d7c512ea823b3134b500a
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-01-09 11:08:57 +0000

    ada/ (saulius@starta) Using big integers (unlimited length) in post-conditions; specifying the Ada_2022 level to access them.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@36 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/spark/gcd.ads
+++ ada/spark/gcd.ads
@@ -1,3 +1,8 @@
+pragma Ada_2022;
+
+with Ada.Numerics.Big_Numbers.Big_Integers;
+use  Ada.Numerics.Big_Numbers.Big_Integers;
+  
 package GCD with Spark_Mode Is
    
    function Max (A, B : Integer) return Integer 
@@ -10,10 +15,13 @@ package GCD with Spark_Mode Is
    function GCD (A, B : in Integer) return Integer
      with
      Pre  => 
-       A > 0 and then A <= Integer'Last and then B > 0 and then
-       B <= Integer'Last,
+       A > 0 and then A <= Integer'Last and then 
+       B > 0 and then B <= Integer'Last,
      Post => 
-       (for some D in 2 .. Max (A,B) => GCD'Result * D = A);
+         (for some D in 2 .. Max (A,B) => 
+            To_Big_Integer (GCD'Result) * To_Big_Integer (D) = 
+            To_Big_Integer (A)
+         );
          
      -- The following postcondition can not be (asily?) proved in
      -- 'gnatprove':
