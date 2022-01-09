ed003d3b510152bbcf7d8ede749f62d422dbff48
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-01-09 08:22:04 +0000

    ada/ (saulius@starta) Using 'and then' in pre-conditions as recommended in https://docs.adacore.com/spark2014-docs/html/ug/en/source/subprogram_contracts.html#subprogram-contracts.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@27 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/spark/gcd.ads
+++ ada/spark/gcd.ads
@@ -2,9 +2,12 @@ package GCD with Spark_Mode Is
 
    function GCD (A, B : in Integer) return Integer
      with
-     Pre  => A > 0 and A <= Integer'Last and B > 0 and B <= Integer'Last,
+     Pre  => 
+       A > 0 and then A <= Integer'Last and then B > 0 and then
+       B <= Integer'Last,
      Post =>
-     GCD'Result > 0 and
-       A mod GCD'Result = 0 and B mod GCD'Result = 0;
+       GCD'Result > 0 and then
+       A mod GCD'Result = 0 and then
+       B mod GCD'Result = 0;
    
 end GCD;
