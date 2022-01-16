4f0fc867d379a86d213b9e0294d666b3f389f5f7
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-01-16 18:26:18 +0000

    ada/ (saulius@starta) Starting to port proof to 'gcd_mod.adb'; expanding definition range of the predicates.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@127 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/spark/gcd_mod.adb
+++ ada/spark/gcd_mod.adb
@@ -2,36 +2,6 @@ pragma Ada_2022;
 
 package body GCD_Mod with Spark_Mode Is
    
-   function Is_GCD (A, B, D : in Positive) return Boolean
-   is 
-      (Is_Divisor (A, D) and then
-         Is_Divisor (B, D) and then
-         (for all D1 in D .. Positive'Last =>
-            D = D1 or else
-            (not Is_Divisor (A, D1)) or else
-            (not Is_Divisor (B, D1))))
-        with
-        Ghost,
-        Pre => A > 0 and then B > 0 and then D > 0,
-        Post =>
-        Is_GCD'Result = 
-          (Is_Divisor (A, D) and then
-             Is_Divisor (B, D) and then
-             (for all D1 in D .. Positive'Last =>
-                D = D1 or else
-                (not Is_Divisor (A, D1)) or else
-                (not Is_Divisor (B, D1))))
-          ;
-   
-   function Is_Common_Divisor (A, B, D : in Positive) return Boolean
-   is 
-      (Is_Divisor (A, D) and then Is_Divisor (B, D))
-        with
-        Ghost,
-        Pre => A > 0 and then B > 0 and then D > 0,
-        Post =>
-        Is_Common_Divisor'Result = (Is_Divisor (A, D) and then Is_Divisor (B, D));
-   
    function GCD (A, B : in Positive) return Positive
    is
       X, Y, T : Natural;
@@ -39,18 +9,52 @@ package body GCD_Mod with Spark_Mode Is
       X := A;
       Y := B;
       
+      --  This is an important assumption, without it the 'gnatprove'
+      --  can not prove the postcondition, and can not prove the
+      --  assumption itself without a hint:      
+      pragma Assume (for all U in Positive =>
+                       (for all V in Positive =>
+                          (for all D in Positive =>
+                             (if U >= D then 
+                                (U mod D - V mod D) mod D = (U - V) mod D))));
+      
+      --  We need to state the every number is a divisor of itself,
+      --  'gnatprove' can not figure it out herself:
+      pragma Assert (for all N in Positive => Is_Divisor (N, N));
+
+      pragma Assert
+        (for all N in Positive =>
+           (for all M in N .. Positive'Last =>
+              (for all G in Positive =>
+                 (if M > N then
+                     (if Is_GCD(M, N, G) then Is_GCD((M - N), N, G)) and then
+                     (if Is_GCD((M - N), N, G) then Is_GCD(M, N, G))
+                 ))));
+      
       while X > 0 loop
-         pragma Loop_Invariant (for all N in Positive => 
-                                  (if Is_Common_Divisor (X, Y, N) then 
-                                      Is_Common_Divisor (A, B, N))
-                               );
-         T := X;
-         X := Y mod X;
-         Y := T;
+         
+         pragma Loop_Invariant
+           (for all G in Positive =>
+              Equivalent (Is_GCD(A, B, G), Is_GCD(X, Y, G)));
+         
+         declare
+            X_Prev : constant Natural := X with Ghost;
+            Y_Prev : constant Natural := Y with Ghost;
+         begin
+            T := X;
+            X := Y mod X;
+            Y := T;
+            
+            pragma Assert
+              (for all G in Positive =>
+                 (if Equivalent (Is_GCD(A, B, G), Is_GCD(X_Prev, Y_Prev, G)) then
+              Equivalent (Is_GCD(A, B, G), Is_GCD(X, Y, G))));
+               
+         end;
       end loop;
       
       pragma Assert (Y > 0);
-      pragma Assert (Is_Common_Divisor (A, B, Y));
+      pragma Assert (Is_GCD (A, B, Y));
       
       return Y;
    end;
