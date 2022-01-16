d57470c7a03c538d01370f1262d4739750e96e48
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-01-16 09:35:40 +0000

    ada/ (saulius@starta) Adding a package two prove (successfully) two main statements about the common divisor.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@85 af456918-7c63-4f43-8f72-c3d2023c81aa

---
+++ ada/spark/gcd_lemmas.adb
@@ -0,0 +1,68 @@
+pragma Ada_2022;
+
+with Ada.Numerics.Big_Numbers.Big_Integers;
+use  Ada.Numerics.Big_Numbers.Big_Integers;
+  
+package body GCD_Lemmas with Spark_Mode Is
+   
+   function Divisor (A,D : Positive) return Boolean
+     is (A mod D = 0)
+     with
+       Ghost,
+       Pre => A in Positive and then D in Positive,
+       Post => Divisor'Result = (A mod D = 0);   
+   
+   function Is_GCD (A, B, D : in Positive) return Boolean
+   is 
+      (Divisor (A, D) and then
+         Divisor (B, D) and then
+         (for all D1 in D .. Positive'Last =>
+            D = D1 or else
+            (not Divisor (A, D1)) or else
+            (not Divisor (B, D1))))
+        with
+        Ghost,
+        Pre => A > 0 and then B > 0 and then D > 0,
+        Post =>
+        Is_GCD'Result = 
+          (Divisor (A, D) and then
+             Divisor (B, D) and then
+             (for all D1 in D .. Positive'Last =>
+                D = D1 or else
+                (not Divisor (A, D1)) or else
+                (not Divisor (B, D1))))
+          ;
+   
+   function Is_Common_Divisor (A, B, D : in Positive) return Boolean
+   is 
+      (Divisor (A, D) and then Divisor (B, D))
+        with
+        Ghost,
+        Pre => A > 0 and then B > 0 and then D > 0,
+        Post =>
+        Is_Common_Divisor'Result = (Divisor (A, D) and then Divisor (B, D));
+        
+        pragma Assume 
+          (for all U in Positive =>
+             (for all V in Positive =>
+                (for all D in Positive =>
+                   (if U >= D then 
+          (U mod D - V mod D) mod D = (U - V) mod D))));
+        
+        pragma Assert 
+          (for all D in Positive => 
+             (for all M in Positive =>
+                (for all N in Positive =>
+                   (if M > N and then Is_Common_Divisor (M, N, D) then 
+          Is_Common_Divisor ((M - N), N, D))))
+          );
+        
+        pragma Assert
+          (for all D in Positive => 
+             (for all M in Positive =>
+                (for all N in Positive =>
+                   (if M > N and then Is_Common_Divisor ((M - N), N, D) then 
+          Is_Common_Divisor (M, N, D))))
+          );    
+   
+end GCD_Lemmas;
---
+++ ada/spark/gcd_lemmas.ads
@@ -0,0 +1,7 @@
+pragma Ada_2022;
+
+package GCD_Lemmas with Spark_Mode Is
+   
+   pragma Elaborate_Body;
+   
+end GCD_Lemmas;
