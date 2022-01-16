02a6d6d98f74e70b3c041518e252b91bd6028279
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-01-16 18:48:18 +0000

    ada/ (saulius@starta) Trying to add assertions that would permit proof of the postcondition, but these assertions themselves can not be proved so far.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@129 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/spark/gcd_mod.adb
+++ ada/spark/gcd_mod.adb
@@ -9,6 +9,24 @@ package body GCD_Mod with Spark_Mode Is
       X := A;
       Y := B;
       
+      pragma Assert
+        (for all N in Natural =>
+           (for all D in Positive =>
+              (if N = D then Is_GCD (N, 0, D))
+           ));
+      
+      pragma Assert
+        (for all N in Natural =>
+           (for all D in Positive =>
+              (if Is_GCD (N, 0, D) then N = D)
+           ));
+      
+      pragma Assert
+        (for all N in Natural =>
+           (for all M in Positive =>
+              (for all D in Positive =>
+                 Equivalent (Is_GCD (N, M, D), Is_GCD (N mod M, M, D)) )));
+      
       while X > 0 loop
          
          pragma Loop_Invariant
@@ -26,7 +44,7 @@ package body GCD_Mod with Spark_Mode Is
             pragma Assert
               (for all G in Positive =>
                  (if Equivalent (Is_GCD(A, B, G), Is_GCD(X_Prev, Y_Prev, G)) then
-              Equivalent (Is_GCD(A, B, G), Is_GCD(X, Y, G))));
+              X = 0 or else Equivalent (Is_GCD(A, B, G), Is_GCD(X, Y, G))));
                
          end;
       end loop;
