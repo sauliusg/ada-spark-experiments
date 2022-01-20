19c002be97381fdc5f87cc12c0e102f1fe0a6ad9
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-01-20 15:59:18 +0000

    ada/ (saulius@tasmanijos-velnias) Staring a GCD proof module using 'for all' quantifiers on big (unlimited) integers.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@148 af456918-7c63-4f43-8f72-c3d2023c81aa

---
+++ ada/spark/gcd-using-forall.ads
@@ -0,0 +1,78 @@
+pragma Ada_2022;
+
+with Ada.Numerics.Big_Numbers.Big_Integers;
+use  Ada.Numerics.Big_Numbers.Big_Integers;
+  
+package GCD with Spark_Mode Is
+   
+   pragma Assert (for all A in Positive =>
+                    (for all D in Positive =>
+                       (if (for some N in Positive =>
+                              (To_Big_Integer(A) =
+                                 To_Big_Integer(D) * To_Big_Integer(N)))
+                          then A mod D = 0)));
+   
+   pragma Assert (for all A in Positive =>
+                    (for all D in Positive =>
+                       (if (A mod D = 0)
+                          then
+     (for some N in Positive =>
+        To_Big_Integer(A) = 
+        To_Big_Integer(D) * To_Big_Integer(N)))));
+
+   function Equivalent (L1, L2 : Boolean) return Boolean
+   is ((L1 and then L2) or else (not L1 and then not L2))
+     with
+     Spark_Mode,
+     Ghost,
+     Pre => L1 in Boolean and L2 in Boolean,
+     Post => Equivalent'Result = ((L1 and L2) or (not L1 and not L2));
+   
+   function Is_Divisor (A,D : Positive) return Boolean
+     is (for some N in Positive =>
+          To_Big_Integer(A) = To_Big_Integer(D) * To_Big_Integer(N))
+     with
+       Ghost,
+       Pre => A in Positive and then D in Positive,
+       Post => Is_Divisor'Result =
+       (for some N in Positive => 
+          To_Big_Integer(A) = To_Big_Integer(D) * To_Big_Integer(N));
+   
+   function Is_Common_Divisor (A, B, D : in Positive) return Boolean
+   is 
+      (Is_Divisor (A, D) and then Is_Divisor (B, D))
+        with
+        Ghost,
+        Pre => A > 0 and then B > 0 and then D > 0,
+        Post =>
+        Is_Common_Divisor'Result = (Is_Divisor (A, D) and then Is_Divisor (B, D));
+   
+   function Is_GCD (A, B, D : in Positive) return Boolean
+   is 
+      (Is_Divisor (A, D) and then
+         Is_Divisor (B, D) and then
+         (for all D1 in D .. Positive'Last =>
+            D = D1 or else
+            (not Is_Divisor (A, D1)) or else
+            (not Is_Divisor (B, D1))))
+        with
+        Ghost,
+        Pre => A > 0 and then B > 0 and then D > 0,
+        Post =>
+        Is_GCD'Result = 
+          (Is_Divisor (A, D) and then
+             Is_Divisor (B, D) and then
+             (for all D1 in D .. Positive'Last =>
+                D = D1 or else
+                (not Is_Divisor (A, D1)) or else
+                (not Is_Divisor (B, D1))))
+          ;
+   
+   function GCD (A, B : in Positive) return Positive
+     with
+     Pre  => 
+       A > 0 and then A <= Positive'Last and then 
+       B > 0 and then B <= Positive'Last,
+     Post => Is_GCD (A, B, GCD'Result);
+         
+end GCD;
