779d4bd60c3713e1a6318d25eff55e268ff51508
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-01-26 18:46:39 +0000

    ada/ (saulius@starta) Even simpler assertion can not be proved...
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@166 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/spark/gcd_using_forall.adb
+++ ada/spark/gcd_using_forall.adb
@@ -79,9 +79,8 @@ package body GCD_Using_Forall with Spark_Mode Is
       
       pragma Assert
         (for all D in Positive => 
-           (for all N in Natural =>
               (for all R in 0 .. D - 1 =>
-                 (BI (N) * BI (D) + BI (R)) mod BI (D) = BI (R))));
+                 (BI (D) + BI (R)) mod BI (D) = BI (R)));
                      
       pragma Assert
         (for all D in Positive => 
