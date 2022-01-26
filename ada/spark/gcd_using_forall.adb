80c811c46ff1cd0d25ac19e40243a5f3f2893ef4
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-01-26 11:17:21 +0000

    ada/ (saulius@tasmanijos-velnias) Could prove Assertion on line 63 with an additional assumption (the same that was used in the "gcd.adb" file).
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@162 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/spark/gcd_using_forall.adb
+++ ada/spark/gcd_using_forall.adb
@@ -12,12 +12,12 @@ package body GCD_Using_Forall with Spark_Mode Is
       --  This is an important assumption, without it the 'gnatprove'
       --  can not prove the postcondition, and can not prove the
       --  assumption itself without a hint:      
-      -- pragma Assume (for all U in Positive =>
-      --                  (for all V in Positive =>
-      --                     (for all D in Positive =>
-      --                        (if U >= D then 
-      --                           (U mod D - V mod D) mod D = (U - V) mod D))));
-      -- 
+      pragma Assume (for all U in Positive =>
+                       (for all V in Positive =>
+                          (for all D in Positive =>
+                             (if U >= D then 
+                                (U mod D - V mod D) mod D = (U - V) mod D))));
+      
       -- pragma Assume (for all U in Positive =>
       --                  (for all V in Positive =>
       --                     (for all D in Positive =>
