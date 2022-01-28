6aca3176fe30c8efc44b52a7a355b154cb894cb1
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-01-28 09:24:55 +0000

    ada/ (saulius@tasmanijos-velnias) Restructuring the matrix processing.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@177 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/arrays/programs/unconstrained_matrices.adb
+++ ada/arrays/programs/unconstrained_matrices.adb
@@ -1,7 +1,7 @@
 with Ada.Text_IO; use Ada.Text_IO;
 -- with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
-procedure Matrix_Multiplication is
+procedure Unconstrained_Matrices is
    
    package Integer_IO is new Ada.Text_IO.Integer_IO (Integer);
    use Integer_IO;
@@ -10,7 +10,36 @@ procedure Matrix_Multiplication is
      array (Integer range <>, Integer range <>) of Integer;
    
    M : Matrix( 1..10, 1..20 );
-
+   U : Matrix( 1..8, 1..8 );
+   
+   N : Integer := 12;
+   
+   procedure Put( M : Matrix ) is
+   begin
+      Put_Line( "[ " );
+      for I in M'Range(1) loop
+         Put( " [ " );
+         for J in M'Range(1) loop
+            Put( M(I,J) );
+            Put( " " );
+         end loop;
+         Put_Line( "]" );
+      end loop;
+      Put_Line( "]" );
+   end Put;
+   
+   procedure Put_Last( M : Matrix ) is
+   begin
+      Put_Line ("The last element of the matrix is:");
+      
+      Put( "I = " ); Put( M'Last(1), 1 ); Put_Line("");
+      Put( "J = " ); Put( M'Last(2), 1 ); Put_Line("");
+      
+      Put( "M(I,J) = " );
+      Put( M(M'Last(1), M'Last(2)), 1 );
+      Put_Line("");
+   end Put_Last;
+   
 begin
    for I in M'Range(1) loop
       for J in M'Range(2) loop
@@ -18,13 +47,24 @@ begin
       end loop;
    end loop;
    
-   Put_Line ("The last element of the matrix is:");
+   for I in U'Range(1) loop
+      for J in U'Range(2) loop
+         if I = J then
+            U(I,J) := 1;
+         else
+            U(I,J) := 0;
+         end if;
+      end loop;
+   end loop;
    
-   Put( "I = " ); Put( M'Last(1), 1 ); Put_Line("");
-   Put( "J = " ); Put( M'Last(2), 1 ); Put_Line("");
+   Put( M );
+   Put_Last( M );
    
-   Put( "M(I,J) = " );
-   Put( M(M'Last(1), M'Last(2)), 1 );
-   Put_Line("");
+   Put_Line( "" );
+   Put_Line( "---------------" );
+   Put_Line( "" );
+   
+   Put( U );
+   Put_Last( U );
    
 end;
