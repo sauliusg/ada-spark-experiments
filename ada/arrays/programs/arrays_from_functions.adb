584ff584a24d0ba3c1d0eb6db39ebce2393c61a6
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-02-23 13:39:19 +0000

    ada/ (saulius@tasmanijos-velnias) Testing how Ada functions can return arrays of different lengths.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@206 af456918-7c63-4f43-8f72-c3d2023c81aa

---
+++ ada/arrays/programs/arrays_from_functions.adb
@@ -0,0 +1,25 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Arrays_From_Functions is
+   
+   package Integer_IO is new Ada.Text_IO.Integer_IO (Integer);
+   use Integer_IO;
+   
+   type Vector is array (Integer range <>) of Integer;
+   
+   function Make_Array( Size : Integer ) return Vector is
+      V : Vector(1..Size);
+   begin
+      for I in V'Range loop
+         V(I) := 0;
+      end loop;
+      return V;
+   end;
+   
+   V : Vector(1..10);
+   
+begin
+   
+   V := Make_Array( 10 );
+   
+end;
