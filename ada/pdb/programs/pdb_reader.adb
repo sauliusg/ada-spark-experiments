f3eeb1294486c22b4b0b4000d5af7ebf07256e3f
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-04-22 08:12:53 +0000

    ada/ (saulius@starta) Adding a project to check how PDB(x) files can be read.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@266 af456918-7c63-4f43-8f72-c3d2023c81aa

---
+++ ada/pdb/programs/pdb_reader.adb
@@ -0,0 +1,8 @@
+with Text_IO;   use Text_IO;
+with PDB_Atoms; use PDB_Atoms;
+
+procedure PDB_Reader is
+   
+begin
+   Put_Line ("Ada PDB Atom Reader");
+end PDB_Reader;
---
+++ ada/pdb/src/pdb_atoms.ads
@@ -0,0 +1,7 @@
+package PDB_Atoms is
+   
+   type PDB_Atom is record
+      X, Y, Z : Float;
+   end record;
+   
+end PDB_Atoms;
