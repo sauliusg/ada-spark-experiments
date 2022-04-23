27f93a0b0774f6c7688612f8a687f0ac1027a5f6
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-04-23 17:02:29 +0000

    ada/ (saulius@starta) IMplementing the "Perly Reader" package, a package that emulates Perl's "<>" operator.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@276 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/pdb/programs/pdb_reader.adb
+++ ada/pdb/programs/pdb_reader.adb
@@ -3,6 +3,8 @@ with Ada.Strings.Fixed;   use Ada.Strings.Fixed;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Float_Text_IO;   use Ada.Float_Text_IO;
 with PDB_Atoms;           use PDB_Atoms;
+with GNAT.Command_Line;   use GNAT.Command_Line;
+with Ada.Command_Line;    use Ada.Command_Line;
 
 procedure PDB_Reader is
    
@@ -10,10 +12,70 @@ procedure PDB_Reader is
    
    Default_Charge : constant Integer := 0;
    
+   package Perly_Reader is
+      function Get_Current_Line return String;
+      function End_Of_All_Files return Boolean;
+   private
+      Is_STDIN : Boolean := False;
+      Current_File : File_Type;
+      Current_Argument : Integer := 0;
+   end Perly_Reader;         
+         
+   package body Perly_Reader is
+      
+      function Get_Current_Line return String is
+      begin
+         if Argument_Count = 0 then
+            return Get_Line (Standard_Input);
+         else
+            if Current_Argument = 0 or else
+              (Is_STDIN and then End_Of_File (Standard_Input)) or else
+              (Current_Argument < Argument_Count and then End_Of_File (Current_File)) then
+               if not Is_STDIN and then Current_Argument > 0 then
+                  Close (Current_File);
+               end if;
+               Current_Argument := Current_Argument + 1;
+               if Argument (Current_Argument) = "-" then
+                  Is_STDIN := True;
+               else
+                  Is_STDIN := False;
+                  Open (Current_File, In_File, Argument (Current_Argument));
+               end if;
+            end if;
+            if Is_STDIN then
+               return Get_Line (Standard_Input);
+            else
+               return Get_Line (Current_File);
+            end if;
+         end if;
+      end;
+      
+      function End_Of_All_Files return Boolean is
+      begin
+         if Argument_Count = 0 then
+            return End_Of_File (Standard_Input);
+         elsif Current_Argument = Argument_Count then
+            if Is_STDIN then
+               return End_Of_File (Standard_Input);
+            else
+               if End_Of_File (Current_File) then
+                  return True;
+               else
+                  return False;
+               end if;
+            end if;
+         end if;
+         return False;
+      end;
+
+   end Perly_Reader;
+   
+   use Perly_Reader;
+   
 begin
-   while not End_Of_File (Standard_Input) loop
+   while not End_Of_All_Files loop
       declare
-         PDB_Line : String := Get_Line (Standard_Input);
+         PDB_Line : String := Get_Current_Line;
       begin
          
          if PDB_Line'Last > 6 and then
