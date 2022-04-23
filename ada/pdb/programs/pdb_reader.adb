96ea040dc4fc2e07d2d358bc29c72dfe5e8324fd
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-04-23 11:21:28 +0000

    ada/ (saulius@starta) Checking type compatibilities.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@270 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/pdb/programs/pdb_reader.adb
+++ ada/pdb/programs/pdb_reader.adb
@@ -5,9 +5,12 @@ procedure PDB_Reader is
    
    A : PDB_Atom;
    
+   Default_Charge : Integer := +1;
+   
 begin
    A.Atomic_Number := 1;
-   A.Charge := -1;
+   Default_Charge := Default_Charge - 1;
+   A.Charge := Default_Charge;
    
    Put_Line ("Ada PDB Atom Reader");
 end PDB_Reader;
