e01069ee9ab6bbe272d70bbe828df02b09d4f726
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-04-24 09:10:07 +0000

    ada/ (saulius@starta) Factoring out the PDB atom printour procedure into a selarate module.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@286 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/pdb/programs/pdb_reader.adb
+++ ada/pdb/programs/pdb_reader.adb
@@ -1,14 +1,9 @@
 with Ada.Text_IO;         use Ada.Text_IO;
-with Ada.Strings.Fixed;   use Ada.Strings.Fixed;
-with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
-with Ada.Float_Text_IO;   use Ada.Float_Text_IO;
-with PDB_Atoms;           use PDB_Atoms;
-
 with Perly_Reader;        use Perly_Reader;
+with PDB_Atoms;           use PDB_Atoms;
+with PDB_Atom_Printer;    use PDB_Atom_Printer;
 
 procedure PDB_Reader is
-   
-   Default_Charge : constant Integer := 0;
    
 begin
    while not End_Of_All_Files loop
@@ -19,47 +14,11 @@ begin
          
          if PDB_Line'Last > 6 and then
            (PDB_Line (1..6) = "ATOM  " or else PDB_Line (1..6) = "HETATM") then
-            A.Charge := Default_Charge;
-         
+            
             Get_Atom (A, PDB_Line);
-         
-            Put (PDB_Atom_Kind'Image (A.Kind));
-            Put ((6 - PDB_Atom_Kind'Image(A.Kind)'Length) * " ");
-            Put (" ");
-            Put (A.Serial_Number, 5);
-            Put (" ");
-            Put (A.Atom_Name);
-            Put (" ");
-            Put (A.Alt_Location);
-            Put (A.Residue_Name);
-            Put (" ");
-            Put (A.Chain);
-            Put (" ");
-            Put (A.Residue_Number, 4);
-            Put (A.Insertion_Code);
-            Put (" ");
-            Put (A.X);
-            Put (" ");
-            Put (A.Y);
-            Put (" ");
-            Put (A.Z);
-            Put (" ");
-            Put (A.Occupancy);
-            Put (" ");
-            Put (A.B_Factor);
-            Put (" ");
-            if A.Segment /= "    " then
-               Put (A.Segment);
-            else
-               Put ("****");
-            end if;
-            Put (" ");
-            Put (A.Chem_Type);
-            Put (" ");
-            Put (Integer (A.Atomic_Number), 3);
-            Put (" ");
-            Put (A.Charge, 2);
+            Print_PDB_Atom (A);
             New_Line;
+            
          end if;
       end;
    end loop;
---
+++ ada/pdb/src/pdb_atom_printer.ads
@@ -0,0 +1,8 @@
+with PDB_Atoms; use PDB_Atoms;
+
+package PDB_Atom_Printer is
+   
+   -- Print all data in the PDB_Atom record for debug purposes.
+   procedure Print_PDB_Atom (A : PDB_Atom);
+   
+end PDB_Atom_Printer;
