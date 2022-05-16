1b76bef63edca470812474355217815f04f60205
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-05-16 06:29:49 +0000

    tests/ (saulius@tasmanijos-velnias) Deleting the 'ada/periodic-table' directory; it is now transferred to its won repository:
    Working Copy Root Path: /home/saulius/src/periodic-table
    URL: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/periodic-table/trunk
    Relative URL: ^/trunk
    Repository Root: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/periodic-table
    Repository UUID: 55a446e8-9e34-486d-ab9c-146d26cb7ca1
    Revision: 33
    Node Kind: directory
    Schedule: normal
    Last Changed Author: saulius
    Last Changed Rev: 33
    Last Changed Date: 2022-05-16 09:27:43 +0300 (Mon, 16 May 2022)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@349 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/periodic-table/src/string_functions.ads
+++
@@ -1,8 +0,0 @@
-package String_Functions is
-   
-   -- Replace all occurences of character 'Chr_From' to 'Chr_To' in
-   --  the entire string 'S':
-   
-   procedure Replace ( S : in out String; Chr_From, Chr_To : Character );
-
-end String_Functions;
