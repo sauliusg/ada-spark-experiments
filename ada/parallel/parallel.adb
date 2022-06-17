c249335c27355c4fc71239cf82122ecb5d31f45b
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-06-17 16:22:00 +0000

    tests/ (saulius@tasmanijos-velnias) Parallel features of the Ada 2022 which are not yet implemented.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@430 af456918-7c63-4f43-8f72-c3d2023c81aa

---
+++ ada/parallel/parallel.adb
@@ -0,0 +1,15 @@
+pragma Ada_2022;
+
+with Text_IO; use Text_IO;
+
+procedure Parallel is
+   
+begin
+   do
+     Put_Line ("This is one parallel thread");
+   and
+     Put_Line ("This is another parallel thread");
+   and
+     Put_Line ("This is yet another parallel thread");
+   end do;
+end;
