6b8de9aa223e33536fcc6cb12a761bd48160824f
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-06-17 17:51:34 +0000

    ada/ (saulius@starta) Fixing the parallel Ada construct syntax.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@431 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/parallel/parallel.adb
+++ ada/parallel/parallel.adb
@@ -5,11 +5,11 @@ with Text_IO; use Text_IO;
 procedure Parallel is
    
 begin
-   do
+   parallel do
      Put_Line ("This is one parallel thread");
    and
      Put_Line ("This is another parallel thread");
    and
      Put_Line ("This is yet another parallel thread");
    end do;
-end;
+end Parallel;
