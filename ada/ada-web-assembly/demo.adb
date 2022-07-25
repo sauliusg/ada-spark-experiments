372d9d1436539c0ed19b9c48cd9d601dd6c5963d
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-07-25 16:40:42 +0000

    tests/ (saulius@tasmanijos-velnias) Moving all Make related tests into the 'GNU-Make/' subdirectory.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@434 af456918-7c63-4f43-8f72-c3d2023c81aa

---
+++ ada/ada-web-assembly/demo.adb
@@ -0,0 +1,57 @@
+-- from:
+-- https://blog.adacore.com/use-of-gnat-llvm-to-translate-ada-applications-to-webassembly
+-- S.G. 2022-07-07
+
+with Web.DOM.Event_Listeners;
+with Web.DOM.Events;
+with Web.HTML.Buttons;
+with Web.HTML.Elements;
+with Web.Strings;
+with Web.Window;
+
+package body Demo is
+
+   function "+" (Item : Wide_Wide_String) return Web.Strings.Web_String
+     renames Web.Strings.To_Web_String;
+
+   type Listener is
+     limited new Web.DOM.Event_Listeners.Event_Listener with null record;
+
+   overriding procedure Handle_Event
+    (Self  : in out Listener;
+     Event : in out Web.DOM.Events.Event'Class);
+
+   L : aliased Listener;
+
+   ------------------
+   -- Handle_Event --
+   ------------------
+
+   overriding procedure Handle_Event
+    (Self  : in out Listener;
+     Event : in out Web.DOM.Events.Event'Class)
+   is
+      X : Web.HTML.Elements.HTML_Element
+        := Web.Window.Document.Get_Element_By_Id (+"toggle_label");
+
+   begin
+      X.Set_Hidden (not X.Get_Hidden);
+   end Handle_Event;
+
+   ---------------------
+   -- Initialize_Demo --
+   ---------------------
+
+   procedure Initialize_Demo is
+      B : Web.HTML.Buttons.HTML_Button
+        := Web.Window.Document.Get_Element_By_Id
+            (+"toggle_button").As_HTML_Button;
+
+   begin
+      B.Add_Event_Listener (+"click", L'Access);
+      B.Set_Disabled (False);
+   end Initialize_Demo;
+
+begin
+   Initialize_Demo;
+end Demo;
