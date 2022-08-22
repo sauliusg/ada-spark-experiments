e3dc55afdd869aa6b632aace8c41cd746ca58542
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-08-22 15:00:07 +0000

    ada/ (saulius@starta) Checking functions with side effects.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@437 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/try-paramaters/try_swap.adb
+++ ada/try-paramaters/try_swap.adb
@@ -14,8 +14,15 @@ procedure Try_Swap is
       X := 43;
    end;
    
+   function Add (X, Y : in Integer; Z : out Integer) return Integer is
+   begin
+      Z := X + Y;
+      return Z;
+   end;
+   
    A : Integer := 12;
    B : Integer := 42;
+   C : Integer;
    
 begin
    -- Init (X =: B); -- does not compile
@@ -29,7 +36,11 @@ begin
    -- Swap ( X :=: A, Y :=: B); -- does not compile
    Swap ( X => A, Y => B);
    
+   Put (Add (A, B, C));
+   New_Line;
+   
    Put ("A = "); Put (A); New_Line;
    Put ("B = "); Put (B); New_Line;
+   Put ("C = "); Put (C); New_Line;
    
 end;
