902b7c1a3e1660fd7e0bfb5c2102848f70679de2
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2022-11-09 18:08:08 +0000

    ada/ (saulius@varanas.saulius-grazulis.lt) More matrix testing code.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@449 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/packages-and-types/test_matrices.adb
+++ ada/packages-and-types/test_matrices.adb
@@ -1,3 +1,4 @@
+with Text_IO;  use Text_IO;
 with Matrices; use Matrices;
 
 procedure Test_Matrices is
@@ -9,8 +10,25 @@ procedure Test_Matrices is
       (4.3, 8.1, 2.7)
      );
    
+   M2 : Matrix :=
+     (
+      (0.1, 0.2, 0.3),
+      (0.4, 0.5, 0.6),
+      (0.7, 0.8, 0.9)
+     );
+   
 begin
    
    Put (M1);
+   New_Line;
+   
+   Put (M2);
+   New_Line;
+   
+   Put (M1 * M2);
+   New_Line;
+   
+   Put (M2 * M1);
+   New_Line;
    
 end Test_Matrices;
