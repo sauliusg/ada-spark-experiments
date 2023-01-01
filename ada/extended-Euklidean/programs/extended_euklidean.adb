69b424573059742adc19d04be1ee60f338f67e88
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-01-01 19:39:13 +0000

    ada/ (saulius@starta) Asing 'Ada.' prefix for the 'Text_IO' package in 'use' and 'with' statements.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@477 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/extended-Euklidean/programs/extended_euklidean.adb
+++ ada/extended-Euklidean/programs/extended_euklidean.adb
@@ -1,4 +1,4 @@
-with Text_IO;             use Text_IO;
+with Ada.Text_IO;         use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Command_Line;    use Ada.Command_Line;
 
