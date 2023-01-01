948be9ecf132a306043099ee4decc75012f93f6d
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-01-01 19:55:50 +0000

    ada/extended-Euklidean/ (saulius@starta) Returning to my previous 'with/use' style with each package statements listed on one line.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@480 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/extended-Euklidean/programs/extended_euklidean.adb
+++ ada/extended-Euklidean/programs/extended_euklidean.adb
@@ -1,10 +1,6 @@
-with Ada.Text_IO;
-with Ada.Integer_Text_IO;
-with Ada.Command_Line;
-
-use Ada.Text_IO;
-use Ada.Integer_Text_IO;
-use Ada.Command_Line;
+with Ada.Text_IO;         use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Command_Line;    use Ada.Command_Line;
 
 procedure Extended_Euklidean is
 
