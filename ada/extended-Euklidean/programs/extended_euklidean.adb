2935c575aaa8e4f2fa984ad4ea5a0107acd870bf
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-01-01 19:48:42 +0000

    ada/ (saulius@starta) Adjusting the code style to the 'adactl -f /usr/share/adacontrol/verif.aru' recommendations.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@478 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/extended-Euklidean/programs/extended_euklidean.adb
+++ ada/extended-Euklidean/programs/extended_euklidean.adb
@@ -1,23 +1,28 @@
-with Ada.Text_IO;         use Ada.Text_IO;
-with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
-with Ada.Command_Line;    use Ada.Command_Line;
+with Ada.Text_IO;
+with Ada.Integer_Text_IO;
+with Ada.Command_Line;
+
+use Ada.Text_IO;
+use Ada.Integer_Text_IO;
+use Ada.Command_Line;
 
 procedure Extended_Euklidean is
-   
+
    -- Test implementaion of the Extended Euclidean Algorithm.
    -- URL: https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
-   
+
    procedure GCD
-     ( 
+     (
       A, B : in Positive; -- original numbers
       D : out Positive;   -- GCD of the two numbers A and B
       M, N : out Integer  -- Bézout coefficients: A * M + B * N = D
-     ) is
+     )
+   is
       P : Integer := 1; -- X = P*A + Q*B at any point
       Q : Integer := 0;
       S : Integer := 0; -- Y = S*A + T*B at any point
       T : Integer := 1;
-      X : Positive := A; 
+      X : Positive := A;
       Y : Positive := B;
    begin
       while X /= Y loop
@@ -37,29 +42,42 @@ procedure Extended_Euklidean is
       -- The naive computation of the N value can overflow:
       -- N := (X - P*A) / B;
       pragma Assert (X = Y);
-      pragma Assert 
+      pragma Assert
         (
          Long_Integer (D) =
-           Long_Integer (A) * Long_Integer (M) + 
+           Long_Integer (A) * Long_Integer (M) +
            Long_Integer (B) * Long_Integer (N)
         );
    end GCD;
-   
+
    A, B : Positive;
    D : Positive;
    M, N : Integer;
 begin -- Extended_Euklidean
    A := Positive'Value (Argument (1));
    B := Positive'Value (Argument (2));
-   
+
    GCD (A, B, D, M, N);
-   
-   Put ("A = "); Put (A); New_Line;
-   Put ("B = "); Put (B); New_Line;
+
+   Put ("A = ");
+   Put (A);
    New_Line;
-   
-   Put ("D = "); Put (D); New_Line;
-   Put ("M = "); Put (M); New_Line;
-   Put ("N = "); Put (N); New_Line;
-   
+
+   Put ("B = ");
+   Put (B);
+   New_Line;
+   New_Line;
+
+   Put ("D = ");
+   Put (D);
+   New_Line;
+
+   Put ("M = ");
+   Put (M);
+   New_Line;
+
+   Put ("N = ");
+   Put (N);
+   New_Line;
+
 end Extended_Euklidean;
