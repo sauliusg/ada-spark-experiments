4a9f74b33dc64563debeee7a3f167108f63b62cd
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-01-01 20:45:38 +0000

    ada/ (saulius@starta) Getting IEEE 754 special values from Ada.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@483 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/floats/special_values.adb
+++ ada/floats/special_values.adb
@@ -1,16 +1,18 @@
 with Ada.Text_IO;       use Ada.Text_IO;
 with Ada.Float_Text_IO; use Ada.Float_Text_IO;
 
-with Ada.Numerics.Generic_Elementary_Functions;
+with Ada.Numerics.Elementary_Functions;
+use Ada.Numerics.Elementary_Functions;
+
+with Ada.Numerics.Complex_Elementary_Functions;
+-- use Ada.Numerics.Complex_Elementary_Functions;
 
 procedure Special_Values is
    
    X : Float :=  1.0;
    Y : Float :=  0.0;
    Z : Float := -1.0;
-   
-   package Math is new Ada.Numerics.Generic_Elementary_Functions (Float);
-   use Math;
+   T : Float;
    
 begin
    
@@ -22,7 +24,13 @@ begin
    Put ("X/Y = "); Put (X/Y);
    New_Line;
    
-   Put ("Sqrt (Z) = "); Put (Sqrt (Z));
+   Put ("Sqrt (Z) = "); Put (Sqrt (X));
+   New_Line;
+   
+   Z := -X/Y;
+   T := X/Y;
+   
+   Put ("X/Y = "); Put (Z/T);
    New_Line;
    
 end Special_Values;
