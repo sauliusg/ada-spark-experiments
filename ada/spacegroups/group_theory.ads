77067108b0f679322c8e9747a74a65c10c1908e3
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-01-18 15:07:04 +0000

    ada/ (saulius@kukutis) Using shorter and more consistent variable names.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@506 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/spacegroups/group_theory.ads
+++ ada/spacegroups/group_theory.ads
@@ -3,44 +3,44 @@ pragma Spark_Mode (On);
 generic
    type Element is private;
    Identity : Element;
-   with function "*" (G1, G2: Element) return Element is <>;
-   with function Inverse (G: Element) return Element;
+   with function "*" (E, F: Element) return Element is <>;
+   with function Inverse (E: Element) return Element;
    
 package Group_Theory is
    
    type Group is array (Integer range <>) of Element;
    
-   function Compute_Inverse (G: Element) return Element
-   is (Inverse (G))
-     with Ghost, Post => G * Compute_Inverse'Result = Identity;
+   function Compute_Inverse (E: Element) return Element
+   is (Inverse (E))
+     with Ghost, Post => E * Compute_Inverse'Result = Identity;
    
    function Is_Identity (I : Element; G : Group) return Boolean
-   is (for all E of G => I * E = Identity and E * I = Identity)
+   is (for all E of G => I * E = E and E * I = E)
      with 
      Ghost,
-     Post => (Is_Identity'Result = (for all E of G => I * E = Identity and E * I = Identity));
+     Post => (Is_Identity'Result = (for all E of G => I * E = E and E * I = E));
    
    function Is_Inverse (A, B : Element) return Boolean
-   is (A * B = Identity)
+   is (A * B = Identity and then B * A = Identity)
      with Ghost;
    
-   function Has_Identity (GG : Group) return Boolean
-   is (for some G of GG => (Is_Identity (G, GG)))
+   function Has_Identity (G : Group) return Boolean
+   is (for some E of G => (Is_Identity (E, G)))
      with Ghost;
    
-   function Has_Inverse (E : Element; GG : Group) return Boolean
-   is (for some G of GG => (E * G = Identity and then G * E = Identity))
+   function Has_Inverse (E : Element; G : Group) return Boolean
+   is (for some H of G => (Is_Inverse (E, H)))
      with Ghost;
    
-   function Belongs_To (E : Element; GG : Group) return Boolean
-   is (for some G of GG => (E = G))
+   function Belongs_To (E : Element; G : Group) return Boolean
+   is (for some F of G => (E = F))
      with Ghost;
    
-   function Is_Group (GG : Group) return Boolean
-   is (Has_Identity (GG) and then
-         (for all E of GG => (Has_Inverse (E, GG))) and then
-         (for all E of GG =>
-            (for all F of GG => (Belongs_To (E*F, GG))))
+   function Is_Group (G : Group) return Boolean
+   is (Has_Identity (G) and then
+         (for all E of G => (Has_Inverse (E, G))) and then
+         (for all E of G =>
+            (for all F of G => (Belongs_To (E*F, G))))
       )
      with Ghost;
    
