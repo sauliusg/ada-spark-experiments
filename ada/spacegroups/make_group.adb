d77c7193fa5082a049636ea0b2687082e2a73cd3
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-01-20 19:29:14 +0000

    ada/ (saulius@starta) Removing one obvious loop invariant.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@526 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/spacegroups/make_group.adb
+++ ada/spacegroups/make_group.adb
@@ -54,7 +54,6 @@ package body Make_Group is
                pragma Assert (for some E of A => (E = X));
                return True;
             end if;
-            pragma Loop_Invariant (K <= A'Last);
             pragma Loop_Invariant (for all I in A'First .. K => (A(I) /= X));
          end loop;
          pragma Assert (for all E of A => (E /= X));
