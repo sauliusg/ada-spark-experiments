3715b74c02c9b4a765d66c00c5528c4d1a666891
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-01-28 13:20:52 +0000

    ada/ (saulius@starta) Using floating point random naumber generator and linear mapping to generate random indices.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@552 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/sudoku/gen_filled_sudoku.adb
+++ ada/sudoku/gen_filled_sudoku.adb
@@ -2,7 +2,7 @@ pragma Ada_2022;
 
 with Ada.Text_IO; use Ada.Text_IO;
 
-with Ada.Numerics.Discrete_Random;
+with Ada.Numerics.Float_Random; use Ada.Numerics.Float_Random;
 
 procedure Gen_Filled_Sudoku is
    
@@ -18,12 +18,7 @@ procedure Gen_Filled_Sudoku is
    Sudoku_Digits : constant Sudoku_Line :=
      (1, 2, 3, 4, 5, 6, 7, 8, 9);
    
-   package Random_Sudoku_Integer is new
-     Ada.Numerics.Discrete_Random (Sudoku_Integer);
-      
-   use Random_Sudoku_Integer;
-   
-   Sudoku_State : Random_Sudoku_Integer.Generator;
+   Sudoku_State : Ada.Numerics.Float_Random.Generator;
    
    Field : Sudoku_Field := (others => (others => 0));
    
@@ -35,12 +30,43 @@ procedure Gen_Filled_Sudoku is
       A := B; B := Tmp;
    end Swap;
    
+   function Random_Less_Than_One (G : Generator) return Uniformly_Distributed is
+      R : Uniformly_Distributed;
+   begin
+      loop
+         R := Random (G);
+         exit when R /= 1.0;
+      end loop;
+      return R;
+   end;
+   
+   function Linear_Map 
+     ( 
+      Rnd : Uniformly_Distributed;
+      First, Last : Sudoku_Integer
+     ) return Sudoku_Integer
+   is
+   begin
+      -- 0          1          2          3          4
+      -- |----------|----------|----------|----------|
+      -- First                                       Last
+      --  <- 0.25 ->|<- 0.50 ->|<- 0.75 ->|<- 1.00 ->|
+      --     => 0       => 1       => 2       => 3
+      return Sudoku_Integer (Natural (First) + Natural (Float'Floor (Float (Last - First) * Rnd)));
+   end Linear_Map;
+   
+   function Random_Index 
+     (
+      G : Generator;
+      First, Last : Sudoku_Integer
+     ) return Sudoku_Integer is
+   begin
+      return Linear_Map (Random_Less_Than_One (G), First, Last);
+   end;
+   
 begin
    
    Reset (Sudoku_State);
-   
-   Put (Random (Sudoku_State)'Image);
-   New_Line;
    
    declare
       Line : Sudoku_Line := Sudoku_Digits;
@@ -49,7 +75,7 @@ begin
       -- Fisher–Yates shuffle:
       -- https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
       for I in Line'First .. Line'Last - 1 loop
-         Swap (Line (I), Line (Random (Sudoku_State)));
+         Swap (Line (I), Line (Random_Index (Sudoku_State, Line'First, Line'Last)));
       end loop;
       for I in Line'Range loop
          Field (1,I) := Line (I);
