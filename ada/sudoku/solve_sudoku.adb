ed95c3cb07e4083f7b49fd8858d7bf16afb0aeda
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-02-01 21:14:35 +0000

    ada/ (saulius@starta) Starting the Sudoku solver.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@585 af456918-7c63-4f43-8f72-c3d2023c81aa

---
+++ ada/sudoku/solve_sudoku.adb
@@ -0,0 +1,18 @@
+with Ada.Command_Line; use Ada.Command_Line;
+with Ada.Text_IO;      use  Ada.Text_IO;
+with Sudoku;           use Sudoku;
+
+procedure Solve_Sudoku is
+   
+   Field : Sudoku_Field;
+   
+   Field_File : File_Type;
+   
+   procedure Load_Field (F : File_Type; S : Sudoku_Field) is
+   begin
+      null;
+   end;
+   
+begin
+   null;
+end Solve_Sudoku;
