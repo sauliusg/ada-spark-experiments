2afebb914bc6cad8329288c2425df9fc28ab99bd
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-02-05 07:47:47 +0000

    ada/ (saulius@starta) Allocatin an initialising just the dunamic array, without a record.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@617 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/types/varying_array.adb
+++ ada/types/varying_array.adb
@@ -15,10 +15,15 @@ procedure Varying_Array is
    
    type Access_Array is access Varying_Array;
    
-   V : Varying_Array (12);
-   
    procedure Free is new
      Unchecked_Deallocation (Varying_Array, Access_Array);
+   
+   type Access_Open_Array is access Open_Array;
+   
+   procedure Free is new
+     Unchecked_Deallocation (Open_Array, Access_Open_Array);
+   
+   V : Varying_Array (12);
    
 begin
    Put_Line (V'Image);
@@ -32,6 +37,23 @@ begin
             A : Access_Array;
          begin
             A := new Varying_Array (I);
+            Put_Line (A.all'Image);
+            Free (A);
+            -- Put_Line (A.all'Image);
+         end;
+      end loop;
+   end;
+   
+   declare
+      Size_Range : array (Integer range <>) of Integer :=
+        (20, 13, 17, 44);
+   begin
+      for I of Size_Range loop
+         declare
+            A : Access_Open_Array;
+         begin
+            A := new Open_Array (1 .. I);
+            A.all := (for V in A.all'First .. A.all'Last => V * V);
             Put_Line (A.all'Image);
             Free (A);
             -- Put_Line (A.all'Image);
