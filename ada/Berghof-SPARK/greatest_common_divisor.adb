9b8f15665bf33a7a245fd87e9e1386cf5c80efe6
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-02-09 13:35:09 +0000

    ada/ (saulius@starta) Adding SPARK example (old syntax?) from Berghofer's "Verification of Dependable Software using Spark and Isabelle".
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@666 af456918-7c63-4f43-8f72-c3d2023c81aa

---
+++ ada/Berghof-SPARK/greatest_common_divisor.adb
@@ -0,0 +1,16 @@
+package body Greatest_Common_Divisor with Spark_Mode
+is
+   procedure G_C_D (M, N : in Natural; G : out Natural)
+   is
+      C, D, R : Natural;
+   begin
+      C := M; D := N;
+      while D /= 0
+        --# assert Gcd (C, D) = Gcd (M, N);
+      loop
+         R := C mod D;
+         C := D; D := R;
+      end loop;
+      G := C;
+   end G_C_D;
+end Greatest_Common_Divisor;
