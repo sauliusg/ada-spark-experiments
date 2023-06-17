d8924a63ea9f7c540a651528636a0711553d3c76
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-06-17 17:45:15 +0000

    ada/spark/ (saulius@starta) Adding lemma to prove that elements of unimodular matrices form a group.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@693 af456918-7c63-4f43-8f72-c3d2023c81aa

---
+++ ada/spark/unimodular/prove_unity_integers_are_group.ads
@@ -0,0 +1,19 @@
+with Unities; use Unities;
+with Group_Theory;
+
+package Prove_Unity_Integers_Are_Group with Spark_Mode is
+   
+   type Unity_Integer_Array is array (1..3) of Unity_Integers;
+   
+   package Unity_Group_Theory is 
+      new Group_Theory (Unity_Integers, Unity_Integers(1));
+   
+   use Unity_Group_Theory;
+   
+   procedure Unity_Integers_Are_Group (A : Unity_Integer_Array)
+     with
+     Ghost,
+     Pre => (A = Unity_Integer_Array'(-1, 0, 1)),
+     Post => (Is_Group (Group (A)));
+   
+end Prove_Unity_Integers_Are_Group;
---
+++ ada/spark/unimodular/unities.ads
@@ -0,0 +1,5 @@
+package Unities with Spark_Mode is
+   
+   type Unity_Integers is new Integer range -1 .. 1;
+   
+end Unities;
