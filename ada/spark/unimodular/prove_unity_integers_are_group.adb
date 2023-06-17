b5e88e02e21c56456ebba77f9c280b4d7ce8ada1
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-06-17 18:24:27 +0000

    ada/spark/ (saulius@starta) Prove for the group-ness of the unities works for the function post-condition, but fails for the lemma.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@697 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/spark/unimodular/prove_unity_integers_are_group.adb
+++ ada/spark/unimodular/prove_unity_integers_are_group.adb
@@ -2,4 +2,11 @@ package body Prove_Unity_Integers_Are_Group with Spark_Mode Is
    
    procedure Unity_Integers_Are_Group (A : Unity_Integer_Array) is null;
    
+   function Make_Full_Group return Group
+   is
+      G : Group (1 .. 2) := (-1, 1);
+   begin
+      return G;
+   end Make_Full_Group;
+
 end Prove_Unity_Integers_Are_Group;
--- ada/spark/unimodular/prove_unity_integers_are_group.ads
+++ ada/spark/unimodular/prove_unity_integers_are_group.ads
@@ -3,7 +3,7 @@ with Group_Theory;
 
 package Prove_Unity_Integers_Are_Group with Spark_Mode is
    
-   type Unity_Integer_Array is array (1..3) of Unity_Integers;
+   type Unity_Integer_Array is array (1..2) of Unity_Integers;
    
    package Unity_Group_Theory is 
       new Group_Theory (Unity_Integers, Unity_Integers(1));
@@ -13,7 +13,11 @@ package Prove_Unity_Integers_Are_Group with Spark_Mode is
    procedure Unity_Integers_Are_Group (A : Unity_Integer_Array)
      with
      Ghost,
-     Pre => (A = Unity_Integer_Array'(-1, 0, 1)),
+     Pre => (A = Unity_Integer_Array'(-1, 1)),
      Post => (Is_Group (Group (A)));
+   
+   function Make_Full_Group return Group
+     with
+     Post => Is_Group (Make_Full_Group'Result);
    
 end Prove_Unity_Integers_Are_Group;
