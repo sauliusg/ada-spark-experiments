f4543eb819b0cb7a65d612ced05899d2a5d1ee1f
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-06-17 18:54:40 +0000

    ada/spark/ (saulius@starta) Adding all three main group properties as the pragma hints, for symmetry. Actually, need both CVC4 and Z3 provers to prove the lemma.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@700 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/spark/unimodular/prove_unity_integers_are_group.adb
+++ ada/spark/unimodular/prove_unity_integers_are_group.adb
@@ -3,7 +3,9 @@ package body Prove_Unity_Integers_Are_Group with Spark_Mode Is
    procedure Unity_Integers_Are_Group (A : Unity_Integer_Array) is
    begin
       pragma Assert (Is_Closed_On_Multiplication (Group (A)));
+      pragma Assert (All_Elements_Have_Inverses (Group (A)));
       pragma Assert (Has_Identity (Group (A)));
+      null;
    end;
    
    function Make_Full_Group return Group
