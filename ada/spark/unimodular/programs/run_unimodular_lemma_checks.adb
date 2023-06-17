4e28297c755ddd86b9fb7234716ca708dc7f85bd
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-06-17 17:55:04 +0000

    ada/spark/ (saulius@starta) Compiling a run-time user of the lemma.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@694 af456918-7c63-4f43-8f72-c3d2023c81aa

---
+++ ada/spark/unimodular/programs/run_unimodular_lemma_checks.adb
@@ -0,0 +1,12 @@
+with Text_IO; use Text_IO;
+
+with Prove_Unity_Integers_Are_Group;
+use Prove_Unity_Integers_Are_Group;
+
+procedure Run_Unimodular_Lemma_Checks is
+   
+begin
+   
+   null;
+   
+end Run_Unimodular_Lemma_Checks;
---
+++ ada/spark/unimodular/prove_unity_integers_are_group.adb
@@ -0,0 +1,5 @@
+package body Prove_Unity_Integers_Are_Group is
+   
+   procedure Unity_Integers_Are_Group (A : Unity_Integer_Array) is null;
+   
+end Prove_Unity_Integers_Are_Group;
