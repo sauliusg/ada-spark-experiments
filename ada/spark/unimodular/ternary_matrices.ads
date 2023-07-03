35b987a2ff8c0513dce87631b15e7d07d06455fa
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-07-03 17:58:14 +0000

    ada/spark/ (saulius@starta) Explicitly stating matrix structure in the post-condition of the matrix multiplication "*" function.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@711 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/spark/unimodular/ternary_matrices.ads
+++ ada/spark/unimodular/ternary_matrices.ads
@@ -7,6 +7,10 @@ package Ternary_Matrices with Spark_Mode is
    
    function "*" (A, B : Ternary_Matrix) return Ternary_Matrix
      with
-     Pre => A'First(2) = B'First(1) and A'Last(2) = B'Last(1);
+     Pre => A'First(2) = B'First(1) and A'Last(2) = B'Last(1),
+     Post => 
+     ("*"'Result'First(1) = A'First(1) and "*"'Result'Last(1) = A'Last(1))
+       and then
+     ("*"'Result'First(2) = B'First(2) and "*"'Result'Last(2) = B'Last(2));
    
 end Ternary_Matrices;
