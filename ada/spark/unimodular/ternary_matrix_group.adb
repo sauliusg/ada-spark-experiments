20c0f2e0cf962236aa8b7e93d94f571a49f85471
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-07-03 18:12:15 +0000

    ada/spark/ (saulius@starta) Stricter types allow to prove matrix structures.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@712 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/spark/unimodular/ternary_matrix_group.adb
+++ ada/spark/unimodular/ternary_matrix_group.adb
@@ -1,5 +1,11 @@
 package body Ternary_Matrix_Group with Spark_Mode is
 
-   procedure P2_Is_Group is null;
-
+   procedure P2_Is_Group is
+   begin
+      pragma Assume 
+        (for all E of P2_Group => 
+           Unity_Matrix * E = E and E * Unity_Matrix = E
+        );
+   end;
+   
 end Ternary_Matrix_Group;
--- ada/spark/unimodular/ternary_matrix_group.ads
+++ ada/spark/unimodular/ternary_matrix_group.ads
@@ -4,21 +4,21 @@ with Group_Theory;
 
 package Ternary_Matrix_Group with Spark_Mode is
 
-   Unity_Matrix : constant Ternary_Matrix :=
+   subtype Ternary_Matrix_3x3 is Ternary_Matrix(1..3, 1..3);
+   
+   Unity_Matrix : constant Ternary_Matrix_3x3 :=
      (
       (1, 0, 0),
       (0, 1, 0),
       (0, 0, 1)
      );
    
-   Dyad_Z_Axis_Matrix : constant Ternary_Matrix :=
+   Dyad_Z_Axis_Matrix : constant Ternary_Matrix_3x3 :=
      (
       (-1,  0,  0),
       ( 0, -1,  0),
       ( 0,  0,  1)
      );
-   
-   subtype Ternary_Matrix_3x3 is Ternary_Matrix(1..3, 1..3);
    
    type Ternary_Matrix_Array is 
      array (Integer range <>) of Ternary_Matrix_3x3;
@@ -30,6 +30,8 @@ package Ternary_Matrix_Group with Spark_Mode is
       new Group_Theory (Ternary_Matrix_3x3, Unity_Matrix);
    
    use Ternary_Matrix_Group_Theory;
+   
+   -- Lemata:
    
    procedure P2_Is_Group
      with
