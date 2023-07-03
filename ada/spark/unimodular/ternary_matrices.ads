205cdb95431e79bd4ef54d40ee6f0d4518caf180
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-07-03 15:51:46 +0000

    Adding definition of type of ternary matrix, matrix that contains only -1, 0 or 1.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@706 af456918-7c63-4f43-8f72-c3d2023c81aa

---
+++ ada/spark/unimodular/ternary_matrices.ads
@@ -0,0 +1,8 @@
+with Unities; use Unities;
+
+package Ternary_Matrices with Spark_Mode is
+
+   type Ternary_Matrix is 
+     array (Integer range <>, Integer range <>) of Unity_Integers;
+   
+end Ternary_Matrices;
