fa6d518281bff20e30b2beaef018d48e5a30b397
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-07-13 12:20:56 +0000

    ada/spark/ (saulius@starta) Printing out matrices via their images.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@725 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/spark/unimodular/programs/run_p2_is_a_group_check.adb
+++ ada/spark/unimodular/programs/run_p2_is_a_group_check.adb
@@ -1,16 +1,25 @@
+pragma Ada_2022;
+
 with Text_IO; use Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
 with Ternary_Matrices;     use Ternary_Matrices;
 with Ternary_Matrix_Group; use Ternary_Matrix_Group;
 
 procedure Run_P2_Is_A_Group_Check is
    
-   M : Ternary_Matrix_3x3;
+   M : Ternary_Matrix_3x3;   
    
 begin
    
    -- null;
    
    M  := Dyad_Z_Axis_Matrix * Dyad_Z_Axis_Matrix;
+   
+   Put (Dyad_Z_Axis_Matrix'Image);
+   New_Line;
+   
+   Put (M'Image);
+   New_Line;
    
 end Run_P2_Is_A_Group_Check;
