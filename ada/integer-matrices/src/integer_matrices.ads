5ae3da05e5331fa83511afa052e0b7cb5ee7e3f9
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-07-24 08:44:15 +0000

    ada/ (saulius@tasmanijos-velnias) Compiling with '-gnata' to check precondictions.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@746 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/integer-matrices/src/integer_matrices.ads
+++ ada/integer-matrices/src/integer_matrices.ads
@@ -1,9 +1,15 @@
-package Integer_Matrices is
+package Integer_Matrices with Spark_Mode Is
    
    type Integer_Matrix is 
      array (Integer range <>, Integer range <>) of Integer;   
    
-   function "*" (A, B : Integer_Matrix) return Integer_Matrix;
+   function "*" (A, B : Integer_Matrix) return Integer_Matrix
+     with
+     Pre => A'First(2) = B'First(1) and A'Last(2) = B'Last(1),
+     Post => 
+     ("*"'Result'First(1) = A'First(1) and "*"'Result'Last(1) = A'Last(1))
+       and then
+     ("*"'Result'First(2) = B'First(2) and "*"'Result'Last(2) = B'Last(2));
       
    procedure Put_Matrix (M : Integer_Matrix);
       
