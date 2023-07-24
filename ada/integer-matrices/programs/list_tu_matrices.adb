96254484df46d302fb723526e2c80344f2b4c9bb
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-07-24 18:25:33 +0000

    ada/ (saulius@starta) Checking also TU property for all generated matrix squares and cubes.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@764 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/integer-matrices/programs/list_tu_matrices.adb
+++ ada/integer-matrices/programs/list_tu_matrices.adb
@@ -64,6 +64,10 @@ begin
       Put (ASCII.HT);
       Put (Boolean'Image(Is_Total_Unimodular(M)));
       Put (ASCII.HT);
+      Put (Boolean'Image(Is_Total_Unimodular(M * M)));
+      Put (ASCII.HT);
+      Put (Boolean'Image(Is_Total_Unimodular(M * M * M)));
+      Put (ASCII.HT);
       Put_Matrix_Line (M);
       New_Line;
       Generate_Matrices (M, Last);
