d8159d9568d28ca8128cebedd824a72533c8a465
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-07-24 07:40:11 +0000

    ada/ (saulius@tasmanijos-velnias) Updating the 'imatmul.adb' code to multiply matrices of arbitrary compatible shape.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@741 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/integer-matrices/programs/imatmul.adb
+++ ada/integer-matrices/programs/imatmul.adb
@@ -4,9 +4,16 @@ with Integer_Matrix_File; use Integer_Matrix_File;
 
 with Ada.Command_Line;          use Ada.Command_Line;
 
+with Ada.Unchecked_Deallocation;
+
 procedure IMatMul is
    
-   Result : access Integer_Matrix;
+   type Access_Integer_Matrix is access Integer_Matrix;
+   
+   Result : Access_Integer_Matrix;
+   
+   procedure Free is 
+      new Ada.Unchecked_Deallocation (Integer_Matrix, Access_Integer_Matrix);
    
 begin
    
@@ -19,11 +26,19 @@ begin
             Result := new Integer_Matrix (M'Range(1), M'Range(2));
             Result.all := M;
          else
-            Result.all := Result.all * M;
+            declare
+               Product : Integer_Matrix := Result.all * M;
+            begin
+               Free (Result);
+               Result := 
+                 new Integer_Matrix (Product'Range (1), Product'Range (2));
+               Result.all := Product;
+            end;
          end if;
       end;
    end loop;
    
    Put_Matrix (Result.all);
    New_Line;
+   Free (Result);
 end;
