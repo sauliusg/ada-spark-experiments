b998670538fd779c6f98397adddbebaf5a04ff26
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-07-24 13:25:29 +0000

    ada/ (saulius@tasmanijos-velnias) Adding a package for lazy inverting a (small) matrix. Ignoring the test *.diff files.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@752 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/integer-matrices/programs/imatinv.adb
+++ ada/integer-matrices/programs/imatinv.adb
@@ -1,11 +1,11 @@
 with Ada.Text_IO;         use Ada.Text_IO;
 with Integer_Matrices;    use Integer_Matrices;
 with Integer_Matrix_File; use Integer_Matrix_File;
-with Lazy_Determinant;    use Lazy_Determinant;
+with Lazy_Inverse;        use Lazy_Inverse;
 
 with Ada.Command_Line;    use Ada.Command_Line;
 
-procedure IMatDet is
+procedure IMatInv is
    
 begin
    
@@ -13,17 +13,19 @@ begin
       declare
          File_Name : String := Argument (I);
          M : Integer_Matrix := Load_Integer_Matrix (File_Name);
-         D : Integer;
+         Inv : Integer_Matrix
+           (
+            M'First(1) .. M'Last(1),
+            M'First(2) .. M'Last(2)
+           );
       begin
          
          pragma Assert (M'First (1) = M'First(2));
          pragma Assert (M'Last (1) = M'Last(2));
          
-         D := Det (M);
+         Inv := Inverse (M);
          
-         Put (Integer'Image (D));
-         Put (ASCII.HT);
-         Put_Matrix_Line (M);
+         Put_Matrix (Inv);
          New_Line;
       end;
    end loop;
---
+++ ada/integer-matrices/src/lazy_inverse.adb
@@ -0,0 +1,29 @@
+with Integer_Matrices; use Integer_Matrices;
+with Lazy_Determinant; use Lazy_Determinant;
+
+package body Lazy_Inverse is
+   
+   function Inverse (M : Integer_Matrix) return Integer_Matrix is
+      Inv : Integer_Matrix
+        (
+         M'First(1) .. M'Last(1),
+         M'First(2) .. M'Last(2)
+        );
+      Coef : Integer := 1;
+      D : Integer;
+   begin
+      D := Det (M);
+      
+      pragma Assert (abs(D) = 1);
+      
+      for I in M'Range(1) loop
+         for J in M'Range(1) loop
+            Inv (I,J) := Coef * Det (Submatrix(M,I,J)) * D;
+            Coef := - Coef;
+         end loop;
+         Coef := (if I mod 2 = 0 then 1 else -1);
+      end loop;
+      return Inv;
+   end;
+   
+end Lazy_Inverse;
