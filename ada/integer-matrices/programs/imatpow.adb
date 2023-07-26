c94b98f6a397ed29e37566fe29e43516520f80d3
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-07-26 14:30:52 +0000

    ada/ (saulius@tasmanijos-velnias) Implementing computations the specified power for all matrices of each file in the 'programs/imatpow.adb' program.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@804 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/integer-matrices/programs/imatpow.adb
+++ ada/integer-matrices/programs/imatpow.adb
@@ -8,31 +8,44 @@ procedure IMatPow is
    
    Power : Integer;
    
+   File : File_Type;
+   
+   Empty_Line : Boolean := False;
+   
 begin
    
    Power := Integer'Value (Argument (1));
    
-   declare
-      File_Name : String := Argument (2);
-      M : Integer_Matrix := Load_Integer_Matrix (File_Name);
-      Result : Integer_Matrix (M'Range(1), M'Range(2));
-   begin
-      for I in Result'Range(1) loop
-         for J in Result'Range(1) loop
-            if I = J then
-               Result (I,J) := 1;
-            else
-               Result (I,J) := 0;
-            end if;
-         end loop;
-      end loop;
+   for I in 2 .. Argument_Count loop
+      Open (File, In_File, Argument (I));
+      Empty_Line := False;
+      while not End_Of_File (File) loop
+         declare
+            M : Integer_Matrix := Load_Integer_Matrix (File);
+            Result : Integer_Matrix (M'Range(1), M'Range(2));
+         begin
+            for I in Result'Range(1) loop
+               for J in Result'Range(1) loop
+                  if I = J then
+                     Result (I,J) := 1;
+                  else
+                     Result (I,J) := 0;
+                  end if;
+               end loop;
+            end loop;
       
-      for I in 1 .. Power loop
-         Result := Result * M;
+            for I in 1 .. Power loop
+               Result := Result * M;
+            end loop;
+            
+            Put_Matrix (Result);
+            Skip_To_Next_Matrix (File, Empty_Line);
+         end;
       end loop;
-      
-      Put_Matrix (Result);
-      New_Line;
-   end;
+      Close (File);
+      if I < Argument_Count then
+         New_Line;
+      end if;
+   end loop;
    
 end;
