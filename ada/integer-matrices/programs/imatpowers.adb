4700eded480a9757bddf861c7befb831c7673f26
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-07-26 09:00:26 +0000

    ada/ (saulius@tasmanijos-velnias) Respecing empty lines in 'programs/imatpowers.adb'.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@785 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/integer-matrices/programs/imatpowers.adb
+++ ada/integer-matrices/programs/imatpowers.adb
@@ -9,36 +9,50 @@ procedure IMatPowers is
    Max_Power : Integer;
    Power : Integer;
    
+   File : File_Type;
+   Empty_Line : Boolean := False;
+   
 begin
    
    Max_Power := Integer'Value (Argument (1));
    
    for I in 2 .. Argument_Count loop
-      declare
-         File_Name : String := Argument (I);
-         M : Integer_Matrix := Load_Integer_Matrix (File_Name);
+      Open (File, In_File, Argument (I));
+      while not End_Of_File (File) loop
+         declare
+            M : Integer_Matrix := Load_Integer_Matrix (File);
+            
+            Unity : Integer_Matrix (M'Range(1), M'Range(2)) :=
+              (others => (others => 0));
          
-         Unity : Integer_Matrix (M'Range(1), M'Range(2)) :=
-           (others => (others => 0));
+            Result : Integer_Matrix := Unity;
+         begin
+            for I in Unity'Range(1) loop
+               Unity (I,I) := 1;
+            end loop;
+   
+            if Empty_Line then
+               New_Line;
+            end if;
+            
+            Result := Unity;
          
-         Result : Integer_Matrix := Unity;
-         
-      begin
-         for I in Unity'Range(1) loop
-            Unity (I,I) := 1;
-         end loop;
-         
-         Result := Unity;
-         
-         for Power in 1 .. Max_Power loop
-            Result := Result * M;      
-            Put (Power'Image);
-            Put (ASCII.HT);
-            Put_Matrix_Line (Result);
-            New_Line;
-            exit when Result = Unity;
-         end loop;
-      end;
+            for Power in 1 .. Max_Power loop
+               Result := Result * M;      
+               Put (Power'Image);
+               Put (ASCII.HT);
+               Put_Matrix_Line (Result);
+               New_Line;
+               exit when Result = Unity;
+            end loop;
+            
+            Skip_To_Next_Matrix (File, Empty_Line);
+         end;
+      end loop;
+      Close (File);
+      if I < Argument_Count then
+         New_Line;
+      end if;
    end loop;
    
 end;
