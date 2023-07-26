9fae27798a955cf9b7a876fc4589a35ddf64119b
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-07-26 14:19:01 +0000

    ada/ (saulius@tasmanijos-velnias) Multiplying all matrices in the input file in 'programs/imatmul.adb'.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@803 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/integer-matrices/programs/imatmul.adb
+++ ada/integer-matrices/programs/imatmul.adb
@@ -15,27 +15,34 @@ procedure IMatMul is
    procedure Free is 
       new Ada.Unchecked_Deallocation (Integer_Matrix, Access_Integer_Matrix);
    
+   File : File_Type;
+   
 begin
    
    for I in 1 .. Argument_Count loop
-      declare
-         File_Name : String := Argument (I);
-         M : Integer_Matrix := Load_Integer_Matrix (File_Name);
-      begin
-         if Result = null then
-            Result := new Integer_Matrix (M'Range(1), M'Range(2));
-            Result.all := M;
-         else
-            declare
-               Product : Integer_Matrix := Result.all * M;
-            begin
-               Free (Result);
-               Result := 
-                 new Integer_Matrix (Product'Range (1), Product'Range (2));
-               Result.all := Product;
-            end;
-         end if;
-      end;
+      Open (File, In_File, Argument (I));
+      while not End_Of_File (File) loop
+         declare
+            File_Name : String := Argument (I);
+            M : Integer_Matrix := Load_Integer_Matrix (File);
+         begin
+            if Result = null then
+               Result := new Integer_Matrix (M'Range(1), M'Range(2));
+               Result.all := M;
+            else
+               declare
+                  Product : Integer_Matrix := Result.all * M;
+               begin
+                  Free (Result);
+                  Result := 
+                    new Integer_Matrix (Product'Range (1), Product'Range (2));
+                  Result.all := Product;
+               end;
+            end if;
+         end;
+         Skip_To_Next_Matrix (File);
+      end loop;
+      Close (File);
    end loop;
    
    Put_Matrix (Result.all);
