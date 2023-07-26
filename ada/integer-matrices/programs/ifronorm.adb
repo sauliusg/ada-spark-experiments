65dbab203050997f867028d6aa6377cd87e9ab1c
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-07-26 06:21:52 +0000

    ada/ (saulius@tasmanijos-velnias) Adding a package to calculate Frobenius norm of the integer matrices in a file.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@779 af456918-7c63-4f43-8f72-c3d2023c81aa

---
+++ ada/integer-matrices/programs/ifronorm.adb
@@ -0,0 +1,44 @@
+with Ada.Text_IO;         use Ada.Text_IO;
+with Integer_Matrices;    use Integer_Matrices;
+with Integer_Matrix_File; use Integer_Matrix_File;
+
+with Ada.Command_Line;    use Ada.Command_Line;
+
+with Ada.Numerics.Long_Long_Elementary_Functions;
+use Ada.Numerics.Long_Long_Elementary_Functions;
+
+procedure IFroNorm is
+   
+   -- Calculate Frobenious norm of all matrices in the input stream.
+   
+   File : File_Type;
+   
+   function Frobenius_Norm (A : Integer_Matrix) return Float is
+      Sum_Of_Squares : Long_Long_Float := 0.0;
+   begin
+      for I in A'Range(1) loop
+         for J in A'Range(2) loop
+            Sum_Of_Squares := Sum_Of_Squares + Long_Long_Float (A(I,J))**2;
+         end loop;
+      end loop;
+      return Float(Sqrt (Sum_Of_Squares));
+   end;
+   
+begin
+   
+   for I in 1 .. Argument_Count loop
+      Open (File, In_File, Argument (I));
+      while not End_Of_File (File) loop
+         declare
+            M : Integer_Matrix := Load_Integer_Matrix (File);
+         begin
+            Put (Float'Image(Frobenius_Norm (M)));
+            Put (ASCII.HT);
+            Put_Matrix_Line (M);
+            New_Line;
+         end;
+      end loop;
+      Close (File);
+   end loop;
+   
+end;
