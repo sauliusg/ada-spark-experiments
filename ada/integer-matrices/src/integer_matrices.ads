8b96039dd63ab8307acbc556c9b64f4348821d23
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-07-26 11:51:30 +0000

    ada/ (saulius@tasmanijos-velnias) Documenting preconditions by raising exceptions with messages.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@795 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/integer-matrices/src/integer_matrices.ads
+++ ada/integer-matrices/src/integer_matrices.ads
@@ -15,23 +15,35 @@ package Integer_Matrices with Spark_Mode Is
 
    function "*" (A, B : Integer_Matrix) return Integer_Matrix
      with
-     Pre => A'First(2) = B'First(1) and A'Last(2) = B'Last(1),
+     Pre => (A'First(2) = B'First(1) and then A'Last(2) = B'Last(1)) or else
+     raise CONSTRAINT_ERROR with
+     "Matrices for multiplication have incompatible structure",
      Post => 
-     ("*"'Result'First(1) = A'First(1) and "*"'Result'Last(1) = A'Last(1))
+     ("*"'Result'First(1) = A'First(1) and then "*"'Result'Last(1) = A'Last(1))
        and then
-     ("*"'Result'First(2) = B'First(2) and "*"'Result'Last(2) = B'Last(2));
+     ("*"'Result'First(2) = B'First(2) and then "*"'Result'Last(2) = B'Last(2));
      
   function "+" (A, B : Integer_Matrix) return Integer_Matrix
     with
     Pre =>
-      A'First(1) = B'First(1) and A'Last(1) = B'Last(1) and
-      A'First(2) = B'First(2) and A'Last(2) = B'Last(2);
+      ((A'First(1) = B'First(1) and then A'Last(1) = B'Last(1)) or else
+        raise CONSTRAINT_ERROR with
+        "Matrices for addition must have the same number of rows")
+            and then
+      ((A'First(2) = B'First(2) and then A'Last(2) = B'Last(2)) or else
+        raise CONSTRAINT_ERROR with
+        "Matrices for addition must have the same number of columns");
     
   function "-" (A, B : Integer_Matrix) return Integer_Matrix
     with
     Pre =>
-      A'First(1) = B'First(1) and A'Last(1) = B'Last(1) and
-      A'First(2) = B'First(2) and A'Last(2) = B'Last(2);
+      ((A'First(1) = B'First(1) and then A'Last(1) = B'Last(1)) or else
+        raise CONSTRAINT_ERROR with
+        "Matrices for subtraction must have the same number of rows")
+            and then
+      ((A'First(2) = B'First(2) and then A'Last(2) = B'Last(2)) or else
+        raise CONSTRAINT_ERROR with
+        "Matrices for subtraction must have the same number of columns");
      
    procedure Put_Matrix (M : Integer_Matrix);
       
