0c5736bf5bba3fc5ad491783f8276408f9cfe0e2
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-08-02 09:59:00 +0000

    ada/spark/ (saulius@kukutis) Printing out feeback from the 'run_unimodular_lemma_checks' program.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@829 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/spark/unimodular/programs/run_unimodular_lemma_checks.adb
+++ ada/spark/unimodular/programs/run_unimodular_lemma_checks.adb
@@ -1,12 +1,19 @@
+pragma Ada_2022;
+
 with Text_IO; use Text_IO;
 
 with Prove_Unity_Integers_Are_Group;
 use Prove_Unity_Integers_Are_Group;
 
+with Unities; use Unities;
+
 procedure Run_Unimodular_Lemma_Checks is
+   
+   A : Unity_Integer_Array := (1, -1);
    
 begin
    
-   null;
+   Unity_Integers_Are_Group (A);
+   Put ("Proved that the " & A'Image & " array elements form a group.");
    
 end Run_Unimodular_Lemma_Checks;
