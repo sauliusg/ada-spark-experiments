0744e82784f8e1fad730f2f50a85308a3a7321e1
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-09-03 08:55:02 +0000

    trunk/ada/ (saulius@starta) Correctly handling cases with spaces.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@868 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/integer-matrices/src/symop_parser.adb
+++ ada/integer-matrices/src/symop_parser.adb
@@ -124,8 +124,8 @@ package body Symop_Parser is
                        ) is
       Coef : Unity_Integers := 1;
    begin
+      Skip_Spaces (S, Pos);
       while Pos <= S'Last and then S(Pos) /= ',' and then S(Pos) /= ' ' loop
-         Skip_Spaces (S, Pos);
          case S (Pos) is
             when '-' => Coef := -1;
             when '+' => Coef := +1;
@@ -149,6 +149,7 @@ package body Symop_Parser is
                  " at position " & Pos'Image;
          end case;
          Pos := Pos + 1;
+         Skip_Spaces (S, Pos);
       end loop;
    end;
    
