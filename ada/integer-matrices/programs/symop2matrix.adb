f3c0ab1fcc87805a215340ab89155c3df5dbf3e4
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-09-03 09:57:35 +0000

    trunk/ada/ (saulius@starta) Separaiting multiple symmetry operation matices with empty lines.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@875 af456918-7c63-4f43-8f72-c3d2023c81aa

--- ada/integer-matrices/programs/symop2matrix.adb
+++ ada/integer-matrices/programs/symop2matrix.adb
@@ -1,3 +1,4 @@
+with Text_IO;            use Text_IO;
 with Integer_Matrices;   use Integer_Matrices;
 with Symmetry_Operation; use Symmetry_Operation;
 with Symop_Parser;       use Symop_Parser;
@@ -7,5 +8,8 @@ procedure Symop2matrix is
 begin
    for I in 1 .. Argument_Count loop
       Put (Parse_Symop (Argument (I)));
+      if I < Argument_Count then
+         New_Line;
+      end if;
    end loop;
 end Symop2matrix;
