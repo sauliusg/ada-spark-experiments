9b65f532be7a5d696f344800d656b1c01ceeb784
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-11-29 20:01:16 +0000

    trunk/ (saulius@starta) Implementing --help and STDIN processing in the first program, 'imattranspose'.
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@931 af456918-7c63-4f43-8f72-c3d2023c81aa

--- integer-matrices/programs/imattranspose.adb
+++ integer-matrices/programs/imattranspose.adb
@@ -3,15 +3,33 @@ with Integer_Matrices;    use Integer_Matrices;
 with Integer_Matrix_File; use Integer_Matrix_File;
 
 with Ada.Command_Line;    use Ada.Command_Line;
+with Option_Processor;    use Option_Processor;
+with File_Selector;       use File_Selector;
 
 procedure IMatTranspose is
    
+   procedure Help (Option_String : String; Position : in out Positive) is
+      procedure P (S : String) renames Put_Line;
+   begin
+      P ("Calculate transpose matrices");
+      P ("USAGE:");
+      P ("    " & Command_Name & " --options input.mat");
+   end;
+   
+   Options : Option_Array :=
+     (
+      1 => Help_Option("-h", "--help", Help'Access)
+     );
+   
 begin
    
-   for I in 1 .. Argument_Count loop
+   Process_Options (Options);
+   
+   for I in 1 .. File_Name_Count loop
       declare
-         File_Name : String := Argument (I);
-         M : Integer_Matrix := Load_Integer_Matrix (File_Name);         
+         subtype File_Access is File_Selector.File_Access;
+         File : File_Access := Select_File (I);
+         M : Integer_Matrix := Load_Integer_Matrix (File.all);
          T : Integer_Matrix := Transpose (M);
       begin
          Put_Matrix (T);
@@ -19,4 +37,7 @@ begin
       end;
    end loop;
    
+exception
+   when HELP_PRINTED => null;
+      
 end;
