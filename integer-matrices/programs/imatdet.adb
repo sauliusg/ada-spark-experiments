bd6b6608249b4f1f4cabd1eb4fd45c77060abc98
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-12-01 17:17:23 +0000

    Adding option processing to 'imatdet'.
    trunk/ (saulius@tasmanijos-velnias)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@943 af456918-7c63-4f43-8f72-c3d2023c81aa

--- integer-matrices/programs/imatdet.adb
+++ integer-matrices/programs/imatdet.adb
@@ -4,15 +4,33 @@ with Integer_Matrix_File; use Integer_Matrix_File;
 with Lazy_Determinant;    use Lazy_Determinant;
 
 with Ada.Command_Line;    use Ada.Command_Line;
+with Option_Processor;    use Option_Processor;
+with File_Selector;       use File_Selector;
 
 procedure IMatDet is
    
+   procedure Help is
+      procedure P (S : String) renames Put_Line;
+   begin
+      P ("Calculate determinant of a matrix");
+      P ("USAGE:");
+      P ("    " & Command_Name & " --options input.mat");
+   end;
+   
+   Options : Option_Array :=
+     (
+      1 => Help_Option("-h", "--help")
+     );
+   
 begin
    
-   for I in 1 .. Argument_Count loop
+   Process_Options (Options, Help'Access);
+   
+   for I in 1 .. File_Name_Count loop
       declare
-         File_Name : String := Argument (I);
-         M : Integer_Matrix := Load_Integer_Matrix (File_Name);
+         subtype File_Access is File_Selector.File_Access;
+         File : File_Access := Select_File (I);
+         M : Integer_Matrix := Load_Integer_Matrix (File.all);
          D : Integer;
       begin
          
@@ -28,4 +46,7 @@ begin
       end;
    end loop;
    
+exception
+   when HELP_PRINTED => null;
+      
 end;
