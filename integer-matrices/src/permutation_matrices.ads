1259e096e0e7b7c3fa31d23577c2fe44f5430145
saulius <saulius@af456918-7c63-4f43-8f72-c3d2023c81aa>
2023-12-13 09:52:54 +0000

    Proving the full invariant composed of separate logical expressions.
    trunk/ (saulius@tasmanijos-velnias)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-spark-experiments/trunk@962 af456918-7c63-4f43-8f72-c3d2023c81aa

--- integer-matrices/src/permutation_matrices.ads
+++ integer-matrices/src/permutation_matrices.ads
@@ -29,12 +29,17 @@ private
    
    function Unity (N : Positive) return Permutation_Matrix_Array
      with
-     Post => Is_Permutation_Matrix (Unity'Result);
+     -- Post => Is_Permutation_Matrix (Unity'Result);
+     -- Post => Each_Row_Has_One_Unity (Unity'Result); -- OK
+     -- Post => Each_Column_Has_One_Unity (Unity'Result); -- OK
+     Post => Each_Column_Has_One_Unity (Unity'Result) and then Each_Column_Has_One_Unity (Unity'Result);
    
    type Permutation_Matrix (N : Positive) is record
       A : Permutation_Matrix_Array (1 .. N, 1 .. N) := Unity (N);
    end record
-     with Type_Invariant => Is_Permutation_Matrix (Permutation_Matrix);
+     -- with Type_Invariant => Is_Permutation_Matrix (Permutation_Matrix);
+     -- with Type_Invariant => Is_Permutation_Matrix (A);
+     with Type_Invariant => Each_Column_Has_One_Unity (Permutation_Matrix.A) and then Each_Column_Has_One_Unity (Permutation_Matrix.A);
       
    function Each_Row_Has_Unity (A : Permutation_Matrix_Array) return Boolean
      with Ghost;
@@ -57,7 +62,8 @@ private
         with Ghost;
    
    function Is_Permutation_Matrix (A : Permutation_Matrix_Array) return Boolean is
-      (Each_Row_Has_One_Unity (A) and Each_Column_Has_One_Unity (A))
+      -- (Each_Row_Has_One_Unity (A) and Each_Column_Has_One_Unity (A))
+      (Each_Row_Has_One_Unity (A) and then Each_Column_Has_One_Unity (A))
         with Ghost;
 
    function Is_Permutation_Matrix (P : Permutation_Matrix) return Boolean is
